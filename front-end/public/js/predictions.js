a111c9382da71c818e9eef8f1aee50e45d811f91
Bhavya Singh <bsa103@sfu.ca>
2022-12-09 16:18:33 +0530

    Removing comments and adding methods to support call

--- front-end/public/js/predictions.js
+++ front-end/public/js/predictions.js
@@ -61,91 +61,91 @@
     Chart.defaults.color = "#6C7293";
     Chart.defaults.borderColor = "#000000";
 
-//     $.post("/getUsersPrediction",
-//         {
-//             data: "dummy",
-//         },
-//         function (data, status) {
-//             var resp = {
-//                 labels: [],
-//                 data: []
-//             };
-//             resp.labels = data.labels;
-//             resp.data = data.data;
-//             var ctx1 = $("#active-users").get(0).getContext("2d");
-//             var myChart1 = new Chart(ctx1, {
-//                 type: "line",
-//                 data: {
-//                 labels: resp.labels,
-//                 datasets: [{
-//                     label: "Users",
-//                     data: resp.data,
-//                     backgroundColor: "rgba(235, 22, 22, .7)",
-//                     fill: true
-//                 }
-//                 ]
-//             },
-//             options: {
-//                 responsive: true
-//                 }
-//             });
+    $.post("/getUsersPrediction",
+        {
+            data: "dummy",
+        },
+        function (data, status) {
+            var resp = {
+                labels: [],
+                data: []
+            };
+            resp.labels = data.labels;
+            resp.data = data.data;
+            var ctx1 = $("#active-users").get(0).getContext("2d");
+            var myChart1 = new Chart(ctx1, {
+                type: "line",
+                data: {
+                labels: resp.labels,
+                datasets: [{
+                    label: "Users",
+                    data: resp.data,
+                    backgroundColor: "rgba(235, 22, 22, .7)",
+                    fill: true
+                }
+                ]
+            },
+            options: {
+                responsive: true
+                }
+            });
 
-//       // Active Users Chart
-//     var ctx1 = $("#active-users").get(0).getContext("2d");
-//     var myChart1 = new Chart(ctx1, {
-//         type: "line",
-//         data: {
-//             labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-//             datasets: [{
-//                     label: "Users",
-//                     data: [15, 30, 55, 65, 60, 80, 95],
-//                     backgroundColor: "rgba(235, 22, 22, .7)",
-//                     fill: true
-//                 }
-//             ]
-//             },
-//         options: {
-//             responsive: true
-//         }
-//     });
+      // Active Users Chart
+    var ctx1 = $("#active-users").get(0).getContext("2d");
+    var myChart1 = new Chart(ctx1, {
+        type: "line",
+        data: {
+            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            datasets: [{
+                    label: "Users",
+                    data: [15, 30, 55, 65, 60, 80, 95],
+                    backgroundColor: "rgba(235, 22, 22, .7)",
+                    fill: true
+                }
+            ]
+            },
+        options: {
+            responsive: true
+        }
+    });
     
-//     // Active Users Chart
-//     var ctx1 = $("#active-users").get(0).getContext("2d");
-//     var myChart1 = new Chart(ctx1, {
-//         type: "line",
-//         data: {
-//             labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-//             datasets: [{
-//                     label: "Users",
-//                     data: [15, 30, 55, 65, 60, 80, 95],
-//                     backgroundColor: "rgba(235, 22, 22, .7)",
-//                     fill: true
-//                 }
-//             ]
-//             },
-//         options: {
-//             responsive: true
-//         }
-//     });
+    // Active Users Chart
+    var ctx1 = $("#active-users").get(0).getContext("2d");
+    var myChart1 = new Chart(ctx1, {
+        type: "line",
+        data: {
+            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            datasets: [{
+                    label: "Users",
+                    data: [15, 30, 55, 65, 60, 80, 95],
+                    backgroundColor: "rgba(235, 22, 22, .7)",
+                    fill: true
+                }
+            ]
+            },
+        options: {
+            responsive: true
+        }
+    });
 
-//     // Active Users Chart
-//     var ctx1 = $("#active-users").get(0).getContext("2d");
-//     var myChart1 = new Chart(ctx1, {
-//         type: "line",
-//         data: {
-//             labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-//             datasets: [{
-//                     label: "Users",
-//                     data: [15, 30, 55, 65, 60, 80, 95],
-//                     backgroundColor: "rgba(235, 22, 22, .7)",
-//                     fill: true
-//                 }
-//             ]
-//             },
-//         options: {
-//             responsive: true
-//         }
-//     });
+    // Active Users Chart
+    var ctx1 = $("#active-users").get(0).getContext("2d");
+    var myChart1 = new Chart(ctx1, {
+        type: "line",
+        data: {
+            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            datasets: [{
+                    label: "Users",
+                    data: [15, 30, 55, 65, 60, 80, 95],
+                    backgroundColor: "rgba(235, 22, 22, .7)",
+                    fill: true
+                }
+            ]
+            },
+        options: {
+            responsive: true
+        }
+    });
     
-// })
+})
 })(jQuery);
